fcc876ae55114536fc079313744ad58008188d28
OleksiiPustilnykgit config --global user.name OleksiiPustilnyk <pustilnyk.oleksii@gmail.com>
2023-05-19 00:33:57 +0300

    added style.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './style.css'
+
+const Header = () => {
+    return <div className="red">Header</div>
+}
+
+const App = () => {
+    return <Header />
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode></React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
