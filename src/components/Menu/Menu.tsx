d3a44cce6d86a6a2ca878667688e1679c86a9a73
OleksiiPustilnykgit config --global user.name OleksiiPustilnyk <pustilnyk.oleksii@gmail.com>
2023-05-28 14:06:38 +0300

    added mob adaptation

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -2,44 +2,49 @@ type Props = {}
 
 const Menu = (props: Props) => {
     return (
-        <div className="menu">
-            <nav>
-                <ul className="menu-list">
-                    <li>
-                        <a href="">Home</a>
-                    </li>
-                    <li className="dropdown">
-                        <div className="menu-dropdown">
-                            <a href="">
-                                Categories
-                                <span className="icon-arrow"></span>
-                            </a>
-                        </div>
-                        <div className="dropdown-content">
-                            <a href="">Style #1</a>
-                            <a href="">Music #2</a>
-                            <a href="">Travel #3</a>
-                            <a href="">Cars #4</a>
-                        </div>
-                    </li>
-                    <li>
-                        <a href="">About</a>
-                    </li>
-                    <li>
-                        <a href="">Contact</a>
-                    </li>
-                    <li>
-                        <div className="favorites">
-                            <a href="">
-                                Favorites
-                                <span className="icon-like"></span>
-                                <div className="number-of-likes">0</div>
-                            </a>
-                        </div>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+        <>
+            <div className="menu">
+                <nav>
+                    <ul className="menu-list">
+                        <li>
+                            <a href="">Home</a>
+                        </li>
+                        <li className="dropdown">
+                            <div className="menu-dropdown">
+                                <a href="">
+                                    Categories
+                                    <span className="icon-arrow"></span>
+                                </a>
+                            </div>
+                            <div className="dropdown-content">
+                                <a href="">Style #1</a>
+                                <a href="">Music #2</a>
+                                <a href="">Travel #3</a>
+                                <a href="">Cars #4</a>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="">About</a>
+                        </li>
+                        <li>
+                            <a href="">Contact</a>
+                        </li>
+                        <li>
+                            <div className="favorites">
+                                <a href="">
+                                    Favorites
+                                    <span className="icon-like"></span>
+                                    <div className="number-of-likes">0</div>
+                                </a>
+                            </div>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="mob-menu">
+                <div className="mob-icon-menu"></div>
+            </div>
+        </>
     )
 }
 export default Menu
