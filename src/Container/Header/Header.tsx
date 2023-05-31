7b2f1c232d3ca3d614d33f023f3c2588b68fac1f
OleksiiPustilnykgit config --global user.name OleksiiPustilnyk <pustilnyk.oleksii@gmail.com>
2023-06-01 01:25:57 +0300

    added MUI

--- src/Container/Header/Header.tsx
+++ src/Container/Header/Header.tsx
@@ -3,18 +3,19 @@ import Logo from 'components/Logo/Logo'
 import Link from 'components/Link/Link'
 
 import './Header.scss'
+import { Container } from '@mui/material'
 
 type Props = {}
 const Header = (props: Props) => {
     return (
         <header className="header">
-            <div className="container">
+            <Container maxWidth="xl">
                 <div className="row-header">
                     <Logo />
                     <Menu />
                     <Link />
                 </div>
-            </div>
+            </Container>
         </header>
     )
 }
