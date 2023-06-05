79999dd65e337596ba1bf71e198906781513099f
OleksiiPustilnykgit config --global user.name OleksiiPustilnyk <pustilnyk.oleksii@gmail.com>
2023-06-05 14:53:58 +0300

    edited

--- src/Container/Header/Header.tsx
+++ src/Container/Header/Header.tsx
@@ -2,8 +2,9 @@ import Menu from 'components/Menu/Menu'
 import Logo from 'components/Logo/Logo'
 import Link from 'components/Link/Link'
 
-import './Header.scss'
 import { Container } from '@mui/material'
+
+import './Header.scss'
 
 type Props = {}
 const Header = (props: Props) => {
