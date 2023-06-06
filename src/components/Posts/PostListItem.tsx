40dac4a77c7f9735b4f5f0be955a3935e394230a
OleksiiPustilnykgit config --global user.name OleksiiPustilnyk <pustilnyk.oleksii@gmail.com>
2023-06-06 13:22:13 +0300

    edited post-author

--- src/components/Posts/PostListItem.tsx
+++ src/components/Posts/PostListItem.tsx
@@ -1,4 +1,5 @@
-import { Card, CardContent } from '@mui/material'
+import { Card, CardContent, Avatar, Grid } from '@mui/material'
+import avatar from 'assets/avatar/avatar-author-mini.png'
 
 import './PostListItem.scss'
 
@@ -19,11 +20,38 @@ const PostListItem = (props: Props) => {
                 <div className="post-like"></div>
                 <div className="post-title">{props.title}</div>
                 <p className="post-content">{props.content}</p>
-                <div className="post-author">
-                    <div className="post-author-avatar"></div>
-                    <div className="post-author-name">Will Smith</div>
-                    <div className="post-author-date">05.06.2023</div>
-                </div>
+                <Grid container spacing={2} className="post-author">
+                    <Grid item md={2.5}>
+                        {/* <div className="icon-quotes"></div> */}
+                        <Avatar
+                            alt="Donald Glover"
+                            src={avatar}
+                            sx={{ width: 50, height: 50 }}
+                            className="author-avatar"
+                        />
+                    </Grid>
+                    <Grid
+                        item
+                        container
+                        md={5}
+                        alignItems="center"
+                        className="author-name"
+                    >
+                        Donald Glover
+                    </Grid>
+                    <Grid item md={1} className="author-space">
+                        <div className="author-icon"></div>
+                    </Grid>
+                    <Grid
+                        item
+                        container
+                        md={3.5}
+                        alignItems="center"
+                        className="author-date"
+                    >
+                        05.06.2023
+                    </Grid>
+                </Grid>
             </CardContent>
         </Card>
     )
