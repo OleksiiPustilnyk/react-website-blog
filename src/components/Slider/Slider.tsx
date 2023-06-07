af7d3db674cfb4ec39d8ba8b1764ed5dbb3aa9d7
OleksiiPustilnykgit config --global user.name OleksiiPustilnyk <pustilnyk.oleksii@gmail.com>
2023-06-07 20:25:14 +0300

    added images for slider

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,21 +1,31 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation, Pagination, A11y } from 'swiper'
+import slideSlider from 'utils/slideSlider'
 
 import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
 
 type Props = {}
 
-const Slider = (props: Props) => {
+const Slider = ({}: Props) => {
     return (
         <Swiper
+            modules={[Navigation, Pagination, A11y]}
             spaceBetween={50}
-            slidesPerView={2}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}
         >
-            <SwiperSlide>Slide 1</SwiperSlide>
-            <SwiperSlide>Slide 2</SwiperSlide>
-            <SwiperSlide>Slide 3</SwiperSlide>
-            <SwiperSlide>Slide 4</SwiperSlide>
+            {slideSlider.map(({ id, image, title }) => (
+                <>
+                    <SwiperSlide key={id} className="slide">
+                        <img src={image} alt={title} />
+                    </SwiperSlide>
+                </>
+            ))}
         </Swiper>
     )
 }
---
+++ src/utils/slideSlider.ts
@@ -0,0 +1,30 @@
+type Slide = {
+    id: number
+    image: string
+    title: string
+}
+
+const slideSlider: Slide[] = [
+    {
+        id: 1,
+        image: '/images/images-slider/image-slide1.jpeg',
+        title: 'image-slide1',
+    },
+    {
+        id: 2,
+        image: '/images/images-slider/image-slide2.jpeg',
+        title: 'image-slide2',
+    },
+    {
+        id: 3,
+        image: '/images/images-slider/image-slide3.jpeg',
+        title: 'image-slide3',
+    },
+    {
+        id: 4,
+        image: '/images/images-slider/image-slide4.jpeg',
+        title: 'image-slide4',
+    },
+]
+
+export default slideSlider
