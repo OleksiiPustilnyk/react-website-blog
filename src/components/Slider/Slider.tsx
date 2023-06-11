ec14b27145eb6f8cb243bdf795ebe34d776554a5
OleksiiPustilnykgit config --global user.name OleksiiPustilnyk <pustilnyk.oleksii@gmail.com>
2023-06-12 01:51:18 +0300

    стилізував слайдер, змінив колір баді

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,28 +1,45 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, Pagination, A11y } from 'swiper'
+import { Navigation, Pagination, A11y, EffectFade, Keyboard } from 'swiper'
 import sliderArray from 'utils/sliderArray'
 
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
 
+import './Slider.scss'
+
 type Props = {}
 
 const Slider = ({}: Props) => {
+    const pagination = {
+        clickable: true,
+        renderBullet: function (index: number, className: string) {
+            return '<span class="' + className + '">' + (index + 10) + '</span>'
+        },
+    }
     return (
         <Swiper
-            modules={[Navigation, Pagination, A11y]}
+            modules={[Navigation, Pagination, A11y, EffectFade, Keyboard]}
             spaceBetween={50}
+            effect={'fade'}
+            fadeEffect={{ crossFade: true }}
+            autoHeight={true}
+            speed={800}
+            // centeredSlides={true}
+            keyboard={{
+                enabled: true,
+            }}
             slidesPerView={1}
-            navigation
             pagination={{ clickable: true }}
+            // direction={'vertical'}
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}
+            className="mySwiper"
         >
             {sliderArray.map(({ id, image, title }) => (
                 <>
                     <SwiperSlide key={id} className="slide">
-                        <img src={image} alt={title} />
+                        <img src={image} alt={title} className="slider-image" />
                     </SwiperSlide>
                 </>
             ))}
--- src/utils/sliderArray.ts
+++ src/utils/sliderArray.ts
@@ -7,22 +7,22 @@ type Slide = {
 const sliderArray: Slide[] = [
     {
         id: 1,
-        image: '/images/images-slider/image-slide1.jpeg',
+        image: '/images/images-slider/image-slide6.jpg',
         title: 'image-slide1',
     },
     {
         id: 2,
-        image: '/images/images-slider/image-slide2.jpeg',
+        image: '/images/images-slider/image-slide12.jpg',
         title: 'image-slide2',
     },
     {
         id: 3,
-        image: '/images/images-slider/image-slide3.jpeg',
+        image: '/images/images-slider/image-slide7.jpg',
         title: 'image-slide3',
     },
     {
         id: 4,
-        image: '/images/images-slider/image-slide4.jpeg',
+        image: '/images/images-slider/image-slide13.jpg',
         title: 'image-slide4',
     },
 ]
