3cf9c06c9db771158cfa6491775d41b766e20c32
OleksiiPustilnykgit config --global user.name OleksiiPustilnyk <pustilnyk.oleksii@gmail.com>
2023-06-19 14:23:37 +0300

    added React Masory

--- src/components/Posts/PostsList.tsx
+++ src/components/Posts/PostsList.tsx
@@ -1,6 +1,7 @@
 import { Grid } from '@mui/material'
 import PostListItem from './PostListItem'
 import postsArray from 'utils/postsArray'
+// import Masonry from 'react-responsive-masonry'
 
 type Props = {}
 const PostsList = (props: Props) => {
@@ -8,7 +9,7 @@ const PostsList = (props: Props) => {
         <>
             <Grid container spacing={4}>
                 {postsArray.map(({ id, image, tag, title, content }) => (
-                    <Grid item xs={12} sm={6} md={4} key={id}>
+                    <Grid item xs={12} sm={3} md={4} key={id}>
                         <PostListItem
                             image={image}
                             tag={tag}
