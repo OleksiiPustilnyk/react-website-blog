6b5188048d940096ff34ad30b11d8b7692534986
OleksiiPustilnykgit config --global user.name OleksiiPustilnyk <pustilnyk.oleksii@gmail.com>
2023-07-10 22:47:38 +0300

    added widgetTags

--- src/components/Widget/WidgetTags.tsx
+++ src/components/Widget/WidgetTags.tsx
@@ -28,6 +28,18 @@ const WidgetTags = (props: Props) => {
                             <span className="icon-xx"></span>
                         </div>
                     </Typography>
+                    <div className="post-tag">
+                        <div className="post-tag-item">music</div>
+                        <div className="post-tag-item">street</div>
+                        <div className="post-tag-item">graffiti</div>
+                        <div className="post-tag-item">cars</div>
+                        <div className="post-tag-item">tattoo</div>
+                        <div className="post-tag-item">rap</div>
+                        <div className="post-tag-item">famous</div>
+                        <div className="post-tag-item">lifestyle</div>
+                        <div className="post-tag-item">games</div>
+                        <div className="post-tag-item">RWB</div>
+                    </div>
                 </Container>
             </CardContent>
         </Card>
